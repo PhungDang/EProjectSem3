2fef0d5475faaa73c8224034f49cbfa3b9b0a1ab
dangluni <dangpro94@gmail.com>
2015-08-05 16:32:12 +0700

    d

---
+++ 1.SourceCode/EProjectMVC/EProjectMVC/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using EProjectMVC.Models;
+
+namespace EProjectMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ 1.SourceCode/EProjectMVC/EProjectMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EProjectMVC.Startup))]
+namespace EProjectMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
